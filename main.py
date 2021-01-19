f83fa49959a4390ac84b315d71c25ad3f5934b8f
delebash <danelebash@hotmail.com>
2021-01-19 04:02:06 -0500

    code cleanup

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 # PyCharm
-# import pydevd_pycharm
-#
-# pydevd_pycharm.settrace('127.0.0.1', port=12345, stdoutToServer=False,
-#                         stderrToServer=False)
+import pydevd_pycharm
+
+pydevd_pycharm.settrace('127.0.0.1', port=12345, stdoutToServer=False,
+                        stderrToServer=False)
 import RLPy
+import PySide2
+from PySide2 import *
 from PySide2.shiboken2 import wrapInstance
 from PySide2.QtCore import QThread
-from PySide2.QtWidgets import QMessageBox
-from PySide2.QtWidgets import *
-from PySide2 import *
 from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtWidgets import QWidget
+
 from subprocess import Popen
 
 avatar = None
-Xchange_path = None
+Xchange_path = "D:\Program Files\Reallusion\iClone 3DXchange 7\Bin\iClone3DXchange.exe"
+
 
 class App:
     """GUI Application using PySide2 widgets"""
@@ -32,6 +35,7 @@ class App:
         # -- Add UI Elements --#
         self.info = QtWidgets.QTextEdit()
         self.mocap_layout.addWidget(self.info)
+        self.info.setPlainText("Default path to 3DXchange  " + Xchange_path)
 
         self.path_3DXchange_button = QtWidgets.QPushButton("Set path to 3DXchange")
         self.path_3DXchange_button.clicked.connect(self.path_3dxchange)
@@ -64,8 +68,6 @@ class App:
 
     def update_character(self):
         global avatar
-        # avatar_list = RLPy.RScene.GetAvatars()
-        # avatar = avatar_list[0]
         selection_list = RLPy.RScene.GetSelectedObjects()
         if len(selection_list) > 0:
             for object in selection_list:  # find first avatar
@@ -88,16 +90,17 @@ class App:
             # print(mesh_name)
             material_list = material_component.GetMaterialNames(mesh_name)
             for material in material_list:
-                substring = "_HAIR"  #hair texture png has 01 and 02 plus HAIR
+                substring = "_HAIR"  # hair texture png has 01 and 02 plus HAIR
                 if substring in material:
-                    image_name = image_name.replace(substring,'')
+                    image_name = image_name.replace(substring, '')
                 else:
                     image_name = "_".join(material.split("_")[:-1])  # remove characters after last _
-                    print("   Material_name   " + material + "   Mesh_name  " + mesh_name + "   Image_name  " + image_name)
-
+                    print(
+                        "   Material_name   " + material + "   Mesh_name  " + mesh_name + "   Image_name  " + image_name)
 
                 image_file = "D:/VroidtoIclone/Shibu/tex_shibu_by_Dan/" + image_name + ".png"
                 result = material_component.LoadImageToTexture(mesh_name, material, texture_channel, image_file)
+
 
     def show_dialog(self):
         self.transfer_vroid_character_dialog.Show()
@@ -106,17 +109,15 @@ class App:
 x = App()
 x.show_dialog()
 
-# file ="F00_000_00_Face_00.png"
-# image_file = "D:/VroidtoIclone/Shibu/tex_shibu_by_Dan/" + file
-#
-#
-#
-# print(image_file)
-# print(mesh_name)
-# print(material_name)
-# print(texture_channel)
-#
-# result = material_component.LoadImageToTexture(mesh_name, "Std_Skin_Head", texture_channel, image_file)
-
-# result = material_component.LoadImageToTexture("CC_Base_Body", "Std_Skin_Head", texture_channel, path)
-# print(result.Success)
+#### Attempt to find Morph Creator button and push programatically ###
+# main_widget = wrapInstance(int(RLPy.RUi.GetMainWindow()), QtWidgets.QWidget)
+# lineEdits = form.findChildren(QtGui.QLineEdit)
+# children = main_widget.findChildren(QtWidgets.QPushButton)
+# children = main_widget.findChildren(QtWidgets.QTabWidget)
+# print(children[0].objectName())
+# print(children[0].text())
+# for child in children:
+#     objName = child.objectName()
+#     self.info.append(objName)
+#     objText = child.text()
+#     self.info.append(objText)
